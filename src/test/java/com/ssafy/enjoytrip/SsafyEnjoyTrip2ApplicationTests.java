da7b4707c6559b41ca582fe65675b0c7752cc222
김아린 <dkfls921@gmail.com>
2023-11-14 17:00:07 +0900

    init project

---
+++ src/test/java/com/ssafy/enjoytrip/SsafyEnjoyTrip2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.enjoytrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyEnjoyTrip2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
